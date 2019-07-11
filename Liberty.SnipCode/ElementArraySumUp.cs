9cf8dc0ec8b8d244029ea2d9c7334a129526387d
badpaybad <badpaybad@gmail.com>
2019-07-11 20:49:44 +0700

    init prj

--- Liberty.SnipCode/ElementArraySumUp.cs
+++ Liberty.SnipCode/ElementArraySumUp.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
-    public class ElementArraySumUp
+public class ElementArraySumUp
     {
         int[][] _main = null;
 
@@ -44,35 +48,4 @@
             return res;
         }
 
-    }
-    
-[TestClass]
-    public class Test
-    {
-        [TestMethod]
-        public void Do1Arr()
-        {
-            var abc = new ElementArraySumUp(new int[][] { new[] { 1, 2, 3 } });
-            Console.WriteLine(string.Join(",", abc.DoSumElement()));
-        }
-        [TestMethod]
-        public void Do2Arr()
-        {
-            var abc = new ElementArraySumUp(new int[][] { new[] { 1, 2, 3 }, new[] { 5, 0 } });
-            Console.WriteLine(string.Join(",", abc.DoSumElement()));
-        }
-
-        [TestMethod]
-        public void Do5Arr()
-        {
-            var abc = new ElementArraySumUp(new int[][]
-            {
-                new[] {5,4,3,2,1 }
-                , new[] { 4,1 }
-                , new[] { 5,0,0 }
-                , new[] { 6,4,2 }
-                , new[] { 1 }
-            });
-            Console.WriteLine(string.Join(",", abc.DoSumElement()));
-        }
     }
