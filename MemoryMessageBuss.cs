f4d431c9d6e85c7c507b9430ce2bb68a39e99dce
badpaybad <badpaybad@gmail.com>
2020-03-04 23:12:55 +0700

    add set expired for the keyname

--- MemoryMessageBuss.cs
+++ MemoryMessageBuss.cs
@@ -1,45 +1,56 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 
-    public class MemoryMessageBuss
+public class MemoryMessageBuss
+{
+    static MemoryMessageBuss _instance;
+    public static MemoryMessageBuss Instance
     {
-        static MemoryMessageBuss _instance;
-        public static MemoryMessageBuss Instance
+        get
         {
-            get
-            {
-                if (_instance != null) return _instance;
-                _instance = new MemoryMessageBuss();
-                return _instance;
-            }
+            if (_instance != null) return _instance;
+            _instance = new MemoryMessageBuss();
+            return _instance;
         }
+    }
 
-        ConcurrentDictionary<string, ConcurrentQueue<object>>
-             _queue = new ConcurrentDictionary<string, ConcurrentQueue<object>>();
+    ConcurrentDictionary<string, ConcurrentQueue<object>>
+         _queue = new ConcurrentDictionary<string, ConcurrentQueue<object>>();
 
-        ConcurrentDictionary<string, ConcurrentStack<object>>
-            _stack = new ConcurrentDictionary<string, ConcurrentStack<object>>();
+    ConcurrentDictionary<string, ConcurrentStack<object>>
+        _stack = new ConcurrentDictionary<string, ConcurrentStack<object>>();
 
-        ConcurrentDictionary<string, object>
-            _cache = new ConcurrentDictionary<string, object>();
+    ConcurrentDictionary<string, object>
+        _cache = new ConcurrentDictionary<string, object>();
 
-        ConcurrentDictionary<string, ConcurrentDictionary<string, Action<object>>>
-            _channelSubscriber = new ConcurrentDictionary<string, ConcurrentDictionary<string, Action<object>>>();
-        ConcurrentDictionary<string, bool> _channelTypeIsQueue = new ConcurrentDictionary<string, bool>();
+    ConcurrentDictionary<string, ConcurrentDictionary<string, Action<object>>>
+        _channelSubscriber = new ConcurrentDictionary<string, ConcurrentDictionary<string, Action<object>>>();
+    ConcurrentDictionary<string, bool> _channelTypeIsQueue = new ConcurrentDictionary<string, bool>();
 
-        Thread _channelThread;
-        private MemoryMessageBuss()
+    ConcurrentDictionary<string, DateTime?> _keyExpire = new ConcurrentDictionary<string, DateTime?>();
+
+    Thread _channelThread;
+    private MemoryMessageBuss()
+    {
+        _channelThread = new Thread(() =>
         {
-            _channelThread = new Thread(() =>
+            while (true)
             {
-                while (true)
+                try
                 {
-                    try
+
+                    ThreadPool.QueueUserWorkItem((o) =>
                     {
-                        foreach (var itm in _channelSubscriber)
+                        List<KeyValuePair<string, ConcurrentDictionary<string, Action<object>>>> tempChannel;
+                        lock (_channelSubscriber)
+                        {
+                            tempChannel = _channelSubscriber.ToList();
+                        }
+                        foreach (var itm in tempChannel)
                         {
                             ThreadPool.QueueUserWorkItem((o) =>
                             {
@@ -49,7 +60,13 @@ using System.Threading;
                                     object data = channelType ? Dequeue(itm.Key) : Pop(itm.Key);
                                     if (data != null)
                                     {
-                                        foreach (var subscriber in itm.Value)
+                                        List<KeyValuePair<string, Action<object>>> tempSubscribe;
+                                        lock (itm.Value)
+                                        {
+                                            tempSubscribe = itm.Value.ToList();
+                                        }
+
+                                        foreach (var subscriber in tempSubscribe)
                                         {
                                             ThreadPool.QueueUserWorkItem((o) =>
                                             {
@@ -60,152 +77,198 @@ using System.Threading;
                                 }
                             });
                         }
-                    }
-                    catch
+
+                    });
+
+                    ThreadPool.QueueUserWorkItem((o) =>
                     {
-                    }
-                    finally
-                    {
-                        Thread.Sleep(1);
-                    }
+                        List<KeyValuePair<string, DateTime?>> tempExpired;
+
+                        lock (_keyExpire)
+                        {
+                            tempExpired = _keyExpire.ToList();
+                        }
+
+                        var listExpired = tempExpired.Where(i => i.Value != null && i.Value.Value < DateTime.Now).ToList();
+
+                        foreach (var item in listExpired)
+                        {
+                            _cache.TryRemove(item.Key, out object cacheVal);
+
+                            _queue.TryRemove(item.Key, out ConcurrentQueue<object> queueVal);
+
+                            _stack.TryRemove(item.Key, out ConcurrentStack<object> stackVal);
+
+                            _channelSubscriber.TryRemove(item.Key, out ConcurrentDictionary<string, Action<object>> oldChannelVal);
+
+                            _channelTypeIsQueue.TryRemove(item.Key, out bool oldTypeVal);
+
+                            _keyExpire.TryRemove(item.Key, out DateTime? oldExpired);
+                        }
+
+                    });
                 }
-
-            });
-
-            _channelThread.Start();
-        }
-
-        public void Set<T>(string key, T data)
-        {
-            _cache[key] = data;
-        }
-
-        public T Get<T>(string key)
-        {
-            return _cache.TryGetValue(key, out object val) && val != null ? (T)val : default(T);
-        }
-
-        public void Enqueue<T>(string queueName, T data)
-        {
-            if (!_queue.TryGetValue(queueName, out ConcurrentQueue<object> queueData))
-            {
-                queueData = new ConcurrentQueue<object>();
-                _queue[queueName] = queueData;
-            }
-
-            queueData.Enqueue(data);
-        }
-
-        public object Dequeue(string queueName)
-        {
-            if (_queue.TryGetValue(queueName, out ConcurrentQueue<object> queueData) && queueData != null)
-            {
-                if (queueData.TryDequeue(out object data) && data != null)
+                catch
                 {
-                    return data;
                 }
-            }
-
-            return null;
-        }
-
-        public T Dequeue<T>(string queueName)
-        {
-            var data = Dequeue(queueName);
-            return data == null ? default(T) : (T)data;
-        }
-
-        public void Push<T>(string stackName, T data)
-        {
-            if (!_stack.TryGetValue(stackName, out ConcurrentStack<object> stackData))
-            {
-                stackData = new ConcurrentStack<object>();
-                _stack[stackName] = stackData;
-            }
-
-            stackData.Push(data);
-        }
-        object Pop(string stackName)
-        {
-            if (_stack.TryGetValue(stackName, out ConcurrentStack<object> stackData) && stackData != null)
-            {
-                if (stackData.TryPop(out object data) && data != null)
+                finally
                 {
-                    return data;
+                    Thread.Sleep(1);
                 }
             }
 
-            return null;
-        }
-        public T Pop<T>(string stackName)
-        {
-            var data = Pop(stackName);
-            return data == null ? default(T) : (T)data;
-        }
+        });
 
-        public void Publish<T>(string channelName, T data)
-        {
-            _channelTypeIsQueue[channelName] = true;
-            Enqueue(channelName, data);
-        }
-
-        public void Subscribe<T>(string channelName, string subscribeName, Action<T> callback)
-        {
-            if (!_channelSubscriber.TryGetValue(channelName, out ConcurrentDictionary<string, Action<object>> subscribers))
-            {
-                subscribers = new ConcurrentDictionary<string, Action<object>>();
-                _channelSubscriber[channelName] = subscribers;
-            }
-
-            subscribers[subscribeName] = (o) =>
-            {
-                if (o == null) callback(default(T));
-                else callback((T)o);
-            };
-        }
-
-        public void PublishUseStack<T>(string stackName, T data)
-        {
-            _channelTypeIsQueue[stackName] = false;
-            Push(stackName, data);
-        }
-
+        _channelThread.Start();
     }
 
+    public void Set<T>(string key, T data, DateTime? expireAt = null)
+    {
+        _cache[key] = data;
 
-    /*
-     //usage
-       MemoryMessageBuss.Instance.Subscribe<string>("channel1", "subscriber1", (id) =>
-                 {
-                     Console.WriteLine("channel1 subscriber1: " + id);
-                 });
+        SetExpire(key, expireAt);
+    }
 
-           MemoryMessageBuss.Instance.Publish<string>("channeltest1", "Temp1: " + counter);
+    public T Get<T>(string key)
+    {
+        return _cache.TryGetValue(key, out object val) && val != null ? (T)val : default(T);
+    }
 
-        //------
+    public void SetExpire(string key, DateTime? expireAt = null)
+    {
+        if (expireAt == null)
+        {
+            _keyExpire.TryRemove(key, out expireAt);
+        }
+        else
+        {
+            _keyExpire[key] = expireAt;
+        }
+    }
 
-         new Thread(()=> {
-                    while (true)
-                    {
-                        var data = MemoryMessageBuss.Instance.Dequeue<string>("queuetest");
-                        Console.WriteLine(data);
-                        Thread.Sleep(1000);
-                    }
-                }).Start();
+    public void Enqueue<T>(string queueName, T data)
+    {
+        if (!_queue.TryGetValue(queueName, out ConcurrentQueue<object> queueData))
+        {
+            queueData = new ConcurrentQueue<object>();
+            _queue[queueName] = queueData;
+        }
 
-           MemoryMessageBuss.Instance.Enqueue<string>("queuetest", "queue1: " + counter);
+        queueData.Enqueue(data);
+    }
 
-        //------
+    public object Dequeue(string queueName)
+    {
+        if (_queue.TryGetValue(queueName, out ConcurrentQueue<object> queueData) && queueData != null)
+        {
+            if (queueData.TryDequeue(out object data) && data != null)
+            {
+                return data;
+            }
+        }
 
-                new Thread(() => {
-                    while (true)
-                    {
-                        var data = MemoryMessageBuss.Instance.Pop<string>("stacktest");
-                        Console.WriteLine(data);
-                        Thread.Sleep(1000);
-                    }
-                }).Start();
+        return null;
+    }
 
-                  MemoryMessageBuss.Instance.Push<string>("stacktest", "stack1: "+ counter);
+    public T Dequeue<T>(string queueName)
+    {
+        var data = Dequeue(queueName);
+        return data == null ? default(T) : (T)data;
+    }
 
-         */
+    public void Push<T>(string stackName, T data)
+    {
+        if (!_stack.TryGetValue(stackName, out ConcurrentStack<object> stackData))
+        {
+            stackData = new ConcurrentStack<object>();
+            _stack[stackName] = stackData;
+        }
+
+        stackData.Push(data);
+    }
+    object Pop(string stackName)
+    {
+        if (_stack.TryGetValue(stackName, out ConcurrentStack<object> stackData) && stackData != null)
+        {
+            if (stackData.TryPop(out object data) && data != null)
+            {
+                return data;
+            }
+        }
+
+        return null;
+    }
+    public T Pop<T>(string stackName)
+    {
+        var data = Pop(stackName);
+        return data == null ? default(T) : (T)data;
+    }
+
+    public void Publish<T>(string channelName, T data)
+    {
+        _channelTypeIsQueue[channelName] = true;
+        Enqueue(channelName, data);
+    }
+
+    public void Subscribe<T>(string channelName, string subscribeName, Action<T> callback)
+    {
+        if (!_channelSubscriber.TryGetValue(channelName, out ConcurrentDictionary<string, Action<object>> subscribers))
+        {
+            subscribers = new ConcurrentDictionary<string, Action<object>>();
+            _channelSubscriber[channelName] = subscribers;
+        }
+
+        subscribers[subscribeName] = (o) =>
+        {
+            if (o == null) callback(default(T));
+            else callback((T)o);
+        };
+    }
+
+    public void PublishUseStack<T>(string stackName, T data)
+    {
+        _channelTypeIsQueue[stackName] = false;
+        Push(stackName, data);
+    }
+
+}
+
+
+/*
+ //usage
+   MemoryMessageBuss.Instance.Subscribe<string>("channel1", "subscriber1", (id) =>
+             {
+                 Console.WriteLine("channel1 subscriber1: " + id);
+             });
+
+       MemoryMessageBuss.Instance.Publish<string>("channeltest1", "Temp1: " + counter);
+
+    //------
+
+     new Thread(()=> {
+                while (true)
+                {
+                    var data = MemoryMessageBuss.Instance.Dequeue<string>("queuetest");
+                    Console.WriteLine(data);
+                    Thread.Sleep(1000);
+                }
+            }).Start();
+
+       MemoryMessageBuss.Instance.Enqueue<string>("queuetest", "queue1: " + counter);
+
+    //------
+
+            new Thread(() => {
+                while (true)
+                {
+                    var data = MemoryMessageBuss.Instance.Pop<string>("stacktest");
+                    Console.WriteLine(data);
+                    Thread.Sleep(1000);
+                }
+            }).Start();
+
+              MemoryMessageBuss.Instance.Push<string>("stacktest", "stack1: "+ counter);
+
+     */
+
+
