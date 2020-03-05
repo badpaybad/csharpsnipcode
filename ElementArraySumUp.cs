8faef7fbf590e93e636d92e869239410c608bfaf
dunp <dunp@omt.vn>
2020-03-05 10:43:41 +0700

    keep only one project csharpsnipcode

---
+++ ElementArraySumUp.cs
@@ -0,0 +1,51 @@
+// using System;
+// using System.Collections.Generic;
+// using System.Data;
+// using System.Linq;
+
+// public class ElementArraySumUp
+//     {
+//         int[][] _main = null;
+
+//         public ElementArraySumUp(int[][] main)
+//         {
+//             _main = main;
+//         }
+
+//         public int[] DoSumElement()
+//         {
+//             if (_main == null | _main.Length == 0) throw new NoNullAllowedException("no element to sum");
+//             foreach (var iarr in _main)
+//             {
+//                 if (iarr == null || iarr.Length == 0) throw new NoNullAllowedException("array with no element");
+//             }
+//             if (_main.Length == 1) return _main[0];// early return no need do , only one array no sum
+
+//             var temp = _main.ToList();
+
+//             do
+//             {
+//                 var newArr = SumElement2Array(temp[0], temp[1]).ToArray();
+//                 temp = temp.Skip(2).ToList();
+
+//                 temp.Add(newArr);
+
+//             } while ((temp.Count > 1));
+
+//             return temp[0];
+//         }
+
+//         List<int> SumElement2Array(int[] arr1, int[] arr2)
+//         {
+//             List<int> res = new List<int>();
+//             foreach (var a1 in arr1)
+//             {
+//                 foreach (var a2 in arr2)
+//                 {
+//                     res.Add(a1 + a2);
+//                 }
+//             }
+//             return res;
+//         }
+
+//     }
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading;
+using System;
+
+namespace csharpsnipcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryMessageBuss.Instance.Subscribe<string>("channel1", "subscriber1", (id) =>
+          {
+              Console.WriteLine("channel1 subscriber1: " + id);
+          });
+
+            new Thread(() =>
+                      {
+                          var counter = 0;
+                          while (true)
+                          {
+                              var data = MemoryMessageBuss.Instance.Dequeue<string>("queuetest");
+                              Console.WriteLine(data);
+                              counter++;
+                              Thread.Sleep(1000);
+                          }
+                      }).Start();
+
+            //------
+
+            new Thread(() =>
+            {
+                var counter = 0;
+                while (true)
+                {
+                    var data = MemoryMessageBuss.Instance.Pop<string>("stacktest");
+                    Console.WriteLine(data);
+                    counter++;
+                    Thread.Sleep(1000);
+                }
+            }).Start();
+
+
+            new Thread(() =>
+            {
+
+                var counter = 0;
+
+                while (true)
+                {
+                    MemoryMessageBuss.Instance.Publish<string>("channel1", "Temp1: " + counter);
+
+                    MemoryMessageBuss.Instance.Enqueue<string>("queuetest", "queue1: " + counter);
+
+                    MemoryMessageBuss.Instance.Push<string>("stacktest", "stack1: " + counter);
+
+                    counter++;
+                    Thread.Sleep(1000);
+                }
+
+            }).Start();
+
+            //------
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
